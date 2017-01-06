ec202623d2166ff152d9d146671d349d9f660ca5
npentella <npentella@gmail.com>
2017-01-06 14:32:38 -0600

    Add styling to selected icons

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 window.addEventListener('keydown', function(e) {
 	var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	var icon = document.querySelector(`div[data-key="${e.keyCode}"]`);
 	if(!audio) { return };
+	icon.classList.add('playing');
 	audio.currentTime = 0;
 	audio.play();
 });
