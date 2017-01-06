2f04cd06f4d11163ef33bbad02a49756849e3e84
npentella <npentella@gmail.com>
2017-01-06 14:14:50 -0600

    restart selected audio on each keydown

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 window.addEventListener('keydown', function(e) {
-	console.log(e.keyCode);
 	var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	console.log(audio);
+	audio.currentTime = 0;
 	audio.play();
 });
